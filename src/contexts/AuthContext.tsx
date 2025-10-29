75d47680ad56b4639a3d887d02334a02a5efa880
JoaoFlavio11 <joaoflaviocl@gmail.com>
2025-10-29 19:57:58 -0300

    ..

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,3 +1,27 @@
 'use client';
 
-import { createContext, useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/src/services/firebase-config";
+import React from 'react';
+
+
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children } : { children: React.ReactNode }) {
+  const [ user, setUser ] = useState<User | null>(null);
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) =>{
+      setUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  
+}
